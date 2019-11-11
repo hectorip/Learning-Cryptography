194a0d4ab657a1b65da19b7e18fede4abd780581
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2019-11-10 23:57:46 -0600

    ECB AES

--- code/aes/block.py
+++ code/aes/block.py
@@ -1,3 +1,8 @@
+"""
+Demuestra el funcionamiento cifrado de un bloque de AES en modo Electronic Codebook
+
+"""
+
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes  # didn't even know this library existed
 from cryptography.hazmat.backends import default_backend
 from binascii import hexlify as h
@@ -6,7 +11,7 @@ from os import urandom
 
 # Generting a random key
 key = urandom(16)
-print("My Key {}".format(h(key).decode('utf-8')))
+print("Key: {}".format(h(key).decode('utf-8')))
 print("%s" % key)
 
 cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
@@ -17,4 +22,9 @@ p = b'\x00'*16
 
 c = encryptor.update(p) + encryptor.finalize()
 
-print("%s=%s" % (h(p), h(c))) 
+print("%s=%s" % (h(p), h(c)))
+
+decryptor = cipher.decryptor()
+p = decryptor.update(c) + decryptor.finalize()
+print("%s=%s"% (h(c), h(p)))
+
---
+++ code/aes/ecb_mode.py
@@ -0,0 +1,5 @@
+"""
+Showing how using Electronic Codebook is insecure
+ECB should never be used because it hints about plaintext that is
+exactly the same.
+"""
