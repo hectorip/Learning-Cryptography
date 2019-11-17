f841087ddefbca9eff324b86cc7284ac50de0973
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2019-11-16 23:59:00 -0600

    Notas MD5

--- code/hashing/md5.py
+++ code/hashing/md5.py
@@ -1,7 +1,15 @@
+# Muestra del algoritmo de hasheo MD5
+# Se demostró que es inseguro desde 2005 y no se debería usar en desarrollos
+# nuevos y reemplazarse de cualquier desarrollo que esté en mantenimiento.
+
+# Encontrar una colisión toma segundos, por lo que es demasiado riesgoso
+
 import hashlib
 
 
-m1 = ''
-m2 = 'a'
-m3 = 'b'
-print(hashlib.md5(m1))
+m1 = b''  # Las funciones de hasheo sólo soportan cadenas binarias
+m2 = b'a'
+m3 = b'b'
+print(m1, hashlib.md5(m1).hexdigest())
+print(m2, hashlib.md5(m2).hexdigest())
+print(m3, hashlib.md5(m3).hexdigest())
